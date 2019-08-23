a92179d79bcf39955d65ed2c6de77988d7f28959
sayoga putu gede <putugedesayoga@gmail.com>
2019-08-23 18:49:09 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<? php
+    require 'vendor/autoload.php';
+    use Carbon\Carbon;
+    $date = Carbon::now();
+    echo $date->toDayDateTimeString();
---
+++ scripts/InstallApp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update -y
+apt-get install apache2 -y
+apt-get install mysql-client -y
+apt-get install php libapache2-mod-php php-mysql -y
