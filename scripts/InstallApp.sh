93d915b13553b571571baa8149783993eb15fe6c
sayoga putu gede <putugedesayoga@gmail.com>
2019-08-26 06:48:14 +0700

    change file InstallApp.sh

--- scripts/InstallApp.sh
+++ scripts/InstallApp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 apt-get update -y
 apt-get install apache2 -y
-apt-get install mysql-client -y
-apt-get install php libapache2-mod-php php-mysql -y
+apt-get install php libapache2-mod-php -y
